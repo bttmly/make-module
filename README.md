05a27f0cd72a93ef4b3e9462b260738963c996a0
Nick Bottomley <nhbottomley@gmail.com>
2015-01-10 16:16:36 -0800

    readme & travis

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# create-module
+# make-module
 
 Create a node module from a string of code, and maybe a file name.
 
-`npm install create-module`
+`npm install make-module`
 
 ```js
 var file = path.join(__dirname, "./some-module.js");
 var code = fs.readFileSync(file).toString();
-var createdModule = crateModule(code, file);
-// find `module.exports` at `createdModule.exports`
+var someModule = makeModule(code, file);
+// find `module.exports` at `someModule.exports`
 ```
--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var path = require("path");
 var fs = require("fs");
 var assert = require("assert");
 
-var createModule = require("..");
+var makeModule = require("..");
 
 var code = fs.readFileSync(joinHere("./example/calc.js")).toString();
 
@@ -10,7 +10,7 @@ function joinHere (where) {
   return path.join(__dirname, where);
 }
 
-describe("createModule()", function () {
+describe("makeModule()", function () {
   it("example works as expected normally", function () {
     var calc = require("./example/calc");
     assert(typeof calc.add === "function");
@@ -19,18 +19,18 @@ describe("createModule()", function () {
 
   it("creates modules from arbitrary code", function () {
     var code = "module.exports = {works: true};"
-    var result = createModule(code);
+    var result = makeModule(code);
     assert(result.exports.works === true);
   });
 
   it("example is able to properly require dependencies given a good absolute path", function () {
-    var result = createModule(code, joinHere("./example/calc.js"));
+    var result = makeModule(code, joinHere("./example/calc.js"));
     assert(typeof result.exports.add === "function");
     assert(typeof result.exports.subtract === "function");
   });
 
   it("joins path to process.cwd() if path not absolute", function () {
-    var result = createModule(code, "test/example/calc.js");
+    var result = makeModule(code, "test/example/calc.js");
     assert(typeof result.exports.add === "function");
     assert(typeof result.exports.subtract === "function");
   });
