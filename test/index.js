8a8832113332ec08a267bbee7c10a6abd961e14f
Nick Bottomley <nhbottomley@gmail.com>
2015-01-10 16:39:21 -0800

    demonstrate return values are from `Module()`

--- test/index.js
+++ test/index.js
@@ -24,16 +24,33 @@ describe("makeModule()", function () {
   });
 
   it("example is able to properly require dependencies given a good absolute path", function () {
-    var result = makeModule(code, joinHere("./example/calc.js"));
+    var result = makeModule(code, joinHere("./example/calc"));
     assert(typeof result.exports.add === "function");
     assert(typeof result.exports.subtract === "function");
   });
 
   it("joins path to process.cwd() if path not absolute", function () {
-    var result = makeModule(code, "test/example/calc.js");
+    var result = makeModule(code, "test/example/calc");
     assert(typeof result.exports.add === "function");
     assert(typeof result.exports.subtract === "function");
   });
+
+  it("returns instances of the node core `Module` constructor", function () {
+    var Module = require("module");
+    var result = makeModule("");
+    assert((result instanceof Module) === true);
+  });
+
+  it("has the expected properties", function () {
+    var result = makeModule(code, "test/example/calc");
+    assert(result.hasOwnProperty("exports"));
+    assert(result.hasOwnProperty("id"));
+    assert(result.hasOwnProperty("parent"));
+    assert(result.hasOwnProperty("filename"));
+    assert(result.hasOwnProperty("loaded"));
+    assert(result.hasOwnProperty("children"));
+    assert(result.hasOwnProperty("paths"));
+  })
 
   it("throws as expected", function () {
     var badCode = "module.exports = {a: 1 b: 1};";
